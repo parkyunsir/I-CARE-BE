ac7f673965cb0e7ffda81758f22d3b6c20241114
psy-er <89902211+psy-er@users.noreply.github.com>
2024-06-01 20:43:26 +0900

    feat : 자녀 entity,dto,repository,controller,service 추가

---
+++ backend/src/main/java/com/example/backend/dto/ChildDTO.java
@@ -0,0 +1,48 @@
+package com.example.backend.dto;
+
+import com.example.backend.model.ChatBotFeedbackEntity;
+import com.example.backend.model.ChildEntity;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.Date;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class ChildDTO {
+
+    private String childId;
+    private String parentId;
+    private String nickname;
+    private String name;
+    private String gender;
+    private Date birth;
+    private int profile_state;
+    public ChildDTO(ChildEntity entity) {
+        this.childId= entity.getChildId();
+        this.parentId = entity.getParentId();
+        this.nickname = entity.getNickname();
+        this.name = entity.getName();
+        this.gender = entity.getGender();
+        this.birth = entity.getBirth();
+        this.profile_state = entity.getProfile_state();
+    }
+
+    public static ChildEntity toEntity(final ChildDTO dto) {
+        return ChildEntity.builder()
+                .childId(dto.getChildId())
+                .parentId(dto.getParentId())
+                .nickname(dto.getNickname())
+                .name(dto.getName())
+                .gender(dto.getGender())
+                .birth(dto.getBirth())
+                .profile_state(dto.getProfile_state())
+                .build();
+    }
+
+
+}
